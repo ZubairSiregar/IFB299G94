c8ce503940bbe589f51487bace3dfe00b607a208
Zubair <zubairsir153@hotmail.com>
2015-10-22 09:06:58 +1000

    commit

--- PptyMgmtSys/PptyMgmtSys/StaffMenu.Designer.cs
+++ PptyMgmtSys/PptyMgmtSys/StaffMenu.Designer.cs
@@ -32,6 +32,8 @@
             this.manageTenantsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.newTenantToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.viewAllTenantsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -41,7 +43,7 @@
             this.manageTenantsToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1065, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(349, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -67,11 +69,31 @@
             this.viewAllTenantsToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
             this.viewAllTenantsToolStripMenuItem.Text = "View All Tenants";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(112, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Tenant Details";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(112, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Properties";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // StaffMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1065, 381);
+            this.ClientSize = new System.Drawing.Size(349, 279);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "StaffMenu";
@@ -90,5 +112,7 @@
         private System.Windows.Forms.ToolStripMenuItem manageTenantsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem newTenantToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem viewAllTenantsToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
